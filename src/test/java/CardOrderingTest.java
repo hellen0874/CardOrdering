8918242741a24542294cdfe46baa44ae5d09f570
Elena Musinova <en.musinova@gmail.com>
2023-08-09 23:52:46 +0300

    Tests for task 2, part 1

--- src/test/java/CardOrderingTest.java
+++ src/test/java/CardOrderingTest.java
@@ -39,6 +39,7 @@ public class CardOrderingTest {
         driver = null;
     }
 
+    // Задание 1
     @Test
     void shouldPassPositiveTest() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Александр");
@@ -47,5 +48,46 @@ public class CardOrderingTest {
         driver.findElement(By.cssSelector("button.button")).click();
         String actual = driver.findElement(By.className("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
+    }
+    // Задание 2.
+   // тест 1 - невозможность отправки некорректного имени
+    @Test
+    void shouldNotTakeIncorrectNameTest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Alexandr");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79007777777");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String actual = driver.findElement(By.className("[data-test-id='Input_invalid']")).getText();
+        assertEquals("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы.", actual.trim());
+    }
+    // тест 2 - невозможность отправки пустого имени
+    @Test
+    void shouldNotTakeEmptyNameTest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79007777777");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'.input_invalid .input__sub]")).getText();
+        assertEquals("Поле обязательно для заполнения", actual.trim());
+    }
+    // тест 3 - невозможность отправки некорректного номера телефона
+    @Test
+    void shouldNotTakeIncorrectPhoneTest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Александр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("7900777777");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String actual = driver.findElement(By.className("[data-test-id='Input_invalid']")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +7912345678.", actual.trim());
+    }
+    // тест 4 - невозможность отправки пустого номера телефона
+    @Test
+    void shouldNotTakeEmptyPhoneTest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Александр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String actual = driver.findElement(By.className("[data-test-id='phone'.input_invalid .input__sub]")).getText();
+        assertEquals("Имя и Фамилия указаны неверно. Допустимы только русские буквы, пробелы и дефисы.", actual.trim());
     }
 }
